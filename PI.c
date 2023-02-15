9abf9cb72127f877b2d44d707a1ccd07d22a3404
gabrielplissart <101575508+gabrielplissart@users.noreply.github.com>
2023-02-15 16:08:31 +0000

    Add files via upload

---
+++ PI.c
@@ -0,0 +1,82 @@
+/**
+  Section: Included Files
+ */
+
+#include <xc.h>
+#include "mcc_generated_files/mcc.h"
+#include "PI.h"
+
+/**
+  Section: PI Module APIs
+ */
+
+static uint8_t sensorHeight = 0;
+static uint8_t setpoint = 80; 
+static int error = 0;
+static float integral = 0;
+static float ki = 0.004;
+static float kp = 0.4;
+static int dutycycle;
+
+uint8_t PI_GetSensorHeight(void) {
+    return sensorHeight;
+}
+
+void PI_SetSetpoint(uint8_t value) {
+    setpoint = value;
+}
+uint8_t PI_GetSetPoint(void) {
+    return setpoint;
+}
+
+
+int PI_GetDutycycle(void) {
+    return dutycycle;
+}
+
+void PI_SetKp(float value) {
+    kp = value;
+}
+float PI_GetKp(void) {
+    return kp;
+}
+
+void PI_SetKi(float value) {
+    ki = value;
+}
+float PI_GetKi(void) {
+    return ki;
+}
+
+void PI(void) {
+    sensorHeight = (uint8_t) (ADC_GetConversionResult() >> 2); //resultaat van ADC (8 bit )
+
+    while (1) {
+        LED_SetHigh();
+        __delay_ms(1000);
+        LED_SetLow();
+        __delay_ms(1000);
+    }
+    
+    
+    error = sensorHeight - setpoint;
+    int propA = kp * error;
+    integral = 0;
+    
+    dutycycle = integral + propA;
+
+    if (sensorHeight > 255) {
+        sensorHeight = 255;
+    }
+    if (sensorHeight < 0) {
+        sensorHeight = 0;
+    }
+    
+            
+
+    PWM5_LoadDutyValue( (uint16_t) dutycycle); // output pwm signaal voor hoogte 10 bit (van 0 tot 0x3FF)
+}
+
+/**
+ End of File
+ */
---
+++ main.c
@@ -0,0 +1,71 @@
+/* Demo project pingpongtoren + hoogtesensor 
+ * 
+ * 
+ * pinout:  RC2 = receiver input
+ *          RC7 = transmitter output
+ *          RB6 = pulse lengte output
+ *          RB4 = pwm output
+ *          RC1 = motor output
+ */
+
+#include "mcc_generated_files/mcc.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdint.h>
+#include "PI.h"
+#include "UART.h"
+
+/*
+                         Main application
+ */
+void main(void) {
+    // initialize the device
+    SYSTEM_Initialize();
+
+    // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    INTERRUPT_GlobalInterruptEnable();
+
+    // Enable the Peripheral Interrupts
+    INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+
+    // Initialiseer de hoogtemeting
+    ADC_SelectChannel(Hoogtesensor);
+    ADC_StartConversion();
+    TMR2_Initialize();
+    TMR2_StartTimer();
+    
+    printf("Hello :) \r\n");
+
+    while (1) {
+
+        uartHandler();
+        
+        // PI moet op een vaste frequentie (elke 33ms) lopen voor de integrator
+        if (TMR0_HasOverflowOccured()) {
+            TMR0_Initialize();
+
+            PI();
+            static uint8_t printCycle = 0; //door static toe te voegen wordt "printCycle" niet elke keer her geinitialiseerd maar behoudt het zijn vorige waarde
+            if (printCycle++ > 30) {
+                printLogs();
+                printCycle = 0;
+            }
+        }
+    }
+    
+ 
+}
+
+/**
+ End of File
+ */
